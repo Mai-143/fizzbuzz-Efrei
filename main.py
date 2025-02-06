4fa2173dca77ea3976e226d280709c0d88c3bb54
Mai <mai@MacBook-Pro-de-Mai.local>
2025-02-06 11:56:44 +0100

    Test Unitaire

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 
-def main():
-    for i in range (1, 101):
+def fizzbuzz():
+    result = []  # Stocker les résultats au lieu d'imprimer
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print('FizzBuzz')
+            result.append('FizzBuzz')
         elif i % 3 == 0:
-            print('Fizz')
+            result.append('Fizz')
         elif i % 5 == 0:
-            print('Buzz')
+            result.append('Buzz')
         else:
-            print(i)
-    print('Hello, World!')
-    return 0
+            result.append(i)
+    return result  # Retourner la liste des résultats
+
 
 if __name__ == '__main__':
-    main()
+    fizzbuzz()
 
---
+++ test_unitaire.py
@@ -0,0 +1,14 @@
+import pytest
+from main import fizzbuzz
+
+def test_fizzbuzz():
+    output = fizzbuzz()
+    
+    assert len(output) == 100  
+    assert output[2] == "Fizz"  
+    assert output[4] == "Buzz"  
+    assert output[14] == "FizzBuzz"  
+    assert output[98] == "Fizz"  
+    assert output[99] == "Buzz"  
+    assert output[0] == 1  
+    assert output[1] == 2 
